8bfa596f58274e7bb6a6e353b3f755019abb7bee
HanuCh@udhary <137854084+hanuchaudhary@users.noreply.github.com>
2025-01-25 13:13:30 +0530

    Changes

---
+++ src/app/components/PurchaseButton.tsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import React from "react";
+import Script from "next/script";
+import { createOrderId } from "@/utils/createOrderId";
+
+export default function PurchaseButton({
+  price,
+  buttonTitle,
+}: {
+  price: number;
+  buttonTitle: string;
+}) {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handlePayment = async () => {
+    setIsLoading(true);
+    try {
+      const orderId: string = await createOrderId(price, "INR");
+      const options = {
+        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+        amount: price * 100,
+        currency: "INR",
+        name: "CrossPostHub",
+        description: "Payment for subscription",
+        order_id: orderId,
+        handler: async function (response: any) {
+          try {
+            const paymentResponse = await axios.post(
+              "/api/payment/verifyOrder",
+              {
+                razorpay_order_id: orderId,
+                razorpay_payment_id: response.razorpay_payment_id,
+                razorpay_signature: response.razorpay_signature,
+              }
+            );
+
+            alert("Payment Successful!");
+            console.log(paymentResponse.data);
+          } catch (error) {
+            alert("Payment verification failed. Please contact support.");
+            console.error(error);
+          }
+        },
+        prefill: {
+          name: "YOUR_NAME", // Replace with dynamic user data
+          email: "YOUR_EMAIL", // Replace with dynamic user data
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+
+      const razorpay = new (window as any).Razorpay(options);
+      razorpay.on("payment.failed", function (response: any) {
+        alert("Payment failed");
+        console.error(response.error);
+      });
+      razorpay.open();
+    } catch (error) {
+      alert("Payment failed. Please try again.");
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={handlePayment} disabled={isLoading}>
+        {isLoading ? "Processing..." : buttonTitle}
+      </button>
+      <Script
+        id="razorpay-checkout-js"
+        src="https://checkout.razorpay.com/v1/checkout.js"
+      />
+    </>
+  );
+}
